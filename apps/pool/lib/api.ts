66690d5fee1ef2c28608c0a790cf1084ee9288d0
Dalia Mora <dalia63ramirez@gmail.com>
2023-07-29 15:34:00 +0000

    Updated dependencies and running test

--- apps/pool/lib/api.ts
+++ apps/pool/lib/api.ts
@@ -78,9 +78,9 @@ export const getPools = async (query?: GetPoolsQuery): Promise<Pool[]> => {
     const pagination: Pagination = query?.pagination
       ? JSON.parse(query.pagination)
       : {
-          pageIndex: 0,
-          pageSize: 20,
-        }
+        pageIndex: 0,
+        pageSize: 20,
+      }
     const fromIndex = pagination.pageIndex * pagination.pageSize
     const toIndex = (pagination.pageIndex + 1) * pagination.pageSize
     const orderBy = ORDER_KEY_MAP[(query?.orderBy || 'liquidityUSD')] || 'reserveUSD'
